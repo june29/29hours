4f7390e1bad6ffb73817e3ee62b47ce6f9ad873b
hmsk <k.hamasaki@gmail.com>
2013-04-14 00:38:53 +0900

    be able to use Array for UserMatcher

--- lib/matcher/user_matcher.rb
+++ lib/matcher/user_matcher.rb
@@ -1,11 +1,11 @@
 class TwentyNineHours
   class UserMatcher < Matcher
-    def initialize(screen_name)
-      @screen_name = screen_name
+    def initialize(screen_names)
+      @screen_names = screen_names
     end
 
     def match?(tweet)
-      @screen_name == tweet["user"]["screen_name"]
+      @screen_names.include?(tweet["user"]["screen_name"])
     end
   end
 end
