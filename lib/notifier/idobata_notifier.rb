4d983f2da3ece04a77d024530a438de0015dafda
june29 <june29.jp@gmail.com>
2014-03-25 10:02:50 +0900

    Improve markup of IdobataNotifier's message

--- lib/notifier/idobata_notifier.rb
+++ lib/notifier/idobata_notifier.rb
@@ -6,7 +6,7 @@ class TwentyNineHours
     end
 
     def notify(title, body, options = {})
-      source = "<div style='margin-bottom: 5px;'><img src='#{options[:icon_url]}' width='24' height='24' style='margin-right: 4px;' />#{title}</div><div style='margin-bottom: 5px;'>#{body}</div><div><a href='#{options[:link_url]}'>#{options[:link_url]}</a></div>"
+      source = "<div><img src='#{options[:icon_url]}' width='24' height='24' />&nbsp;<span>#{title}</span></div><div>#{body}</div><div><a href='#{options[:link_url]}'>#{options[:link_url]}</a></div>"
 
       @client.post(@url, source: source, format: "html")
     end
